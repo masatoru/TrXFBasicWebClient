64f35b14afa3cbf82926ef639a2b604f12050f18
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-02-18 11:10:18 +0900

    WinForm: DetailFormの初期化時にデータを表示する

--- TrXFBasicWebClient.WinForms/Views/DetailForm.Designer.cs
+++ TrXFBasicWebClient.WinForms/Views/DetailForm.Designer.cs
@@ -123,6 +123,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "DetailForm";
             this.Text = "DetailForm";
+            this.Load += new System.EventHandler(this.DetailForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- TrXFBasicWebClient.WinForms/Views/DetailForm.cs
+++ TrXFBasicWebClient.WinForms/Views/DetailForm.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XFBasicWebClient.Models;
 
 namespace TrXFBasicWebClient.WinForms.Views
 {
     public partial class DetailForm : Form
     {
-        public DetailForm()
+        private Person Current { get; set; }
+
+        public DetailForm(Person person)
         {
             InitializeComponent();
+            Current = person;
+        }
+
+        /// <summary>
+        /// フォームの初期化時に値をセットする
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DetailForm_Load(object sender, EventArgs e)
+        {
+            if (Current != null)
+            {
+                edtId.Text = Current.Id.ToString();
+                edtName.Text = Current.Name;
+                edtBirthday.Text = Current.Birthday.ToString("yyyy/MM/dd");
+            }
+            else
+            {
+                edtId.Text = "0";
+            }
         }
     }
 }
