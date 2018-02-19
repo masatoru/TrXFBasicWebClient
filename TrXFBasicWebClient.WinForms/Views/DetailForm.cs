5834551a8d150870762ca723a3bde88d7f933ad4
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-02-19 11:43:22 +0900

    Commit Test

--- TrXFBasicWebClient.WinForms/Views/DetailForm.cs
+++ TrXFBasicWebClient.WinForms/Views/DetailForm.cs
@@ -69,7 +69,7 @@ namespace TrXFBasicWebClient.WinForms.Views
         }
 
         /// <summary>
-        /// 削除する
+        /// 削除する TEST
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
